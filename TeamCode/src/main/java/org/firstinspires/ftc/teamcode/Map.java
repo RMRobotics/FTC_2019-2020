645b12c1f04e9ca99c75e96b16ec1a597f395e4a
efang05 <efang05@users.noreply.github.com>
2019-11-05 16:19:22 -0500

    Update Map.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Map.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Map.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Map {
+    private boolean[][] CoordsArr;
+
+    public Map() {
+        CoordsArr = new boolean[24][24];
+
+    }
+    private void AddValues() {
+        for (int x = 0; x < 24; x++) {
+            for(int y = 0; y <24; y++){
+                CoordsArr[x][y] = false;
+            }
+
+
+        }
+    }
 }
