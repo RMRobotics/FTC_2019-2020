8d74c2439d0095e898b359e9456bd85cae56030c
carolynl23 <56048088+carolynl23@users.noreply.github.com>
2019-11-08 21:08:08 -0500

    RoboticsDemo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/recruitAutoTesting.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous()
+
+public abstract class recruitAutoTesting extends LinearOpMode {
+
+    DcMotor FL;
+    DcMotor FR;
+    DcMotor BL;
+    DcMotor BR;
+
+    public void initialize(lift) {
+        DcMotor Lift;
+
+    }
+
+
+    @Override
+
+    public void initialize (Boolean i) {
+        FL = hardwareMap.dcMotor.get("FL");
+        FR = hardwareMap.dcMotor.get("FR");
+        BL = hardwareMap.dcMotor.get("BL");
+        BR = hardwareMap.dcMotor.get("BR");
+
+        FL.setPower(1);
+        FR.setPower(1);
+        BL.setPower(1);
+        BR.setPower(1);
+
+        sleep(200);
+
+        FL.setPower(1);
+        FR.setPower(1);
+        BL.setPower(1);
+        BR.setPower(1);
+
+        sleep(200);
+
+        FL.setPower(0);
+        FR.setPower(0);
+        BL.setPower(0);
+        BR.setPower(0);
+    }
+
+
+}
