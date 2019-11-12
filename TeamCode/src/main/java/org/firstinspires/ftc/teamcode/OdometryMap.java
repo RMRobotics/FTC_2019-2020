bdee217c691895fc19c9eb0ea253265a3dcd2d2b
DanWilliams24 <01danielwilliams@gmail.com>
2019-11-12 14:38:17 -0500

    updated Odometry Map

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdometryMap.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+public class OdometryMap {
+    private Square[][] map;
+
+    public OdometryMap() {
+        map = new Square[24][24];
+
+    }
+
+    private void AddValues() {
+        for (int x = 0; x < 24 ; x++) {
+            for(int y = 0; y <24; y++){
+
+            }
+        }
+    }
+}
